034b35c1dc2608e3c93bce1d0ccad7ab4008c8b3
HsDodo <179621078@qq.com>
2021-05-08 17:58:46 +0800

    Site updated: 2021-05-08 17:58:32

--- js/randombg.js
+++ js/randombg.js
@@ -2,7 +2,7 @@
 var backimg =[
     "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/2.jpg)"
   ];
-  //获取背景图片总数，生成随机数
+  //获取背景图片总数，生成随机数 Math.random() * (backimg.length-1)
   var bgindex =Math.ceil(Math.random() * (backimg.length-1));
   //重设背景图片
   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
@@ -10,10 +10,14 @@ var backimg =[
   var bannerimg =[
     "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/6.jpg)",
     "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/4.jpg)",
-    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/5.jpg)"
-
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/5.jpg)",
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/20210508170657.jpg)",
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/20210507235034.jpg)",
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/20210508170700.jpg)",
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/20210508171936.jpg)",
+    "url(https://cdn.jsdelivr.net/gh/HsDodo/blogImage/images/20210508171935.jpg)"
   ];
-  //获取banner图片总数，生成随机数
+  //获取banner图片总数，生成随机数 Math.random() * (bannerimg.length-1)
   var bannerindex =Math.ceil(Math.random() * (bannerimg.length-1));
   //重设banner图片
   document.getElementById("page-header").style.backgroundImage = bannerimg[bannerindex];
